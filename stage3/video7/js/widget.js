0de936ec391fe045e207b4fe6968b30cadc4a400
Anna <piladzisi@gmail.com>
2019-02-09 15:47:00 +0200

    widgets

--- stage3/video7/js/widget.js
+++ stage3/video7/js/widget.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $.getJSON('../data/employees.json', function (data) {
+  $.getJSON('data/employees.json', function (data) {
     var statusHTML = '<ul class="bulleted">';
     $.each(data,function (index, employee) {
       if (employee.inoffice === true) {
@@ -7,9 +7,25 @@ $(document).ready(function () {
       } else {
         statusHTML +='<li class="out">';
       }
-      statusHTML += employee.name + '</li>';
+      statusHTML += `${employee.name} </li>`;
     });
     statusHTML += '</ul>';
     $('#employeeList').html(statusHTML)
+  }); // end getJSON
+}); // end ready
+
+$(document).ready(function () {
+  $.getJSON('data/rooms.json', function (data) {
+    var statusHTML = '<ul class="rooms">';
+    $.each(data,function (index, room) {
+      if (room.available === true) {
+        statusHTML +='<li class="empty">';
+      } else {
+        statusHTML +='<li class="full">';
+      }
+      statusHTML += `${room.room} </li>`;
+    });
+    statusHTML += '</ul>';
+    $('#roomList').html(statusHTML)
   }); // end getJSON
 }); // end ready
